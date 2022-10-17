079ad429e9bd02b0db9fbb8f0d4e625ca00a1c17
Rumaisamaryam-CS <61551670+Rumaisamaryam-CS@users.noreply.github.com>
2022-10-17 16:54:27 +0500

    Add files via upload

---
+++ lib/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
